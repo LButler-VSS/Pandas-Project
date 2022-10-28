5a6916aeac8da5b6e35961dcbcfe7dd218a6a2c2
Logan Butler <logan3528@gmail.com>
2022-10-28 15:57:44 -0600

    started csv reading process

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from pickletools import read_decimalnl_long
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
+import threading
+import os, os.path
 
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
 
 print(s)
+
+class Reader(threading.Thread):
+    """ This is a dealer that receives cars """
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        for i in range(len(os.listdir('./dataset'))):
+            pd.read_csv()
+        pass
+
+def main():
+    reader = Reader()
+
+    print(len(os.listdir('./dataset')))
+
+    reader.start()
+    reader.join()
+
+
+
+if __name__ == '__main__':
+
+    main()
